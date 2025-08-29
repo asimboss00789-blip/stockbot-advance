ce2638d9e83c9593da701f83e7b22122d25a44b4
asimboss00789-blip <asimboss00789@gmail.com>
2025-08-29 15:50:50 +0300

    Update chat-list.tsx

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -1,19 +1,15 @@
 import { Separator } from '@/components/ui/separator'
 import { UIState } from '@/lib/chat/actions'
 import { Session } from '@/lib/types'
-import Link from 'next/link'
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
 
-export interface ChatList {
+export interface ChatListProps {
   messages: UIState
   session?: Session
   isShared: boolean
 }
 
-export function ChatList({ messages, session, isShared }: ChatList) {
-  if (!messages.length) {
-    return null
-  }
+export function ChatList({ messages, session, isShared }: ChatListProps) {
+  if (!messages.length) return null
 
   return (
     <div className="relative mx-auto max-w-2xl px-4">
